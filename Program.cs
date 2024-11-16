19aec57f8f786a89f1f62ed148da778e5f87aa36
rumeysaexe <rumeysa.main@gmail.com>
2024-11-16 22:45:54 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace çam_agacı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int satirSayisi = 6;
+            for (int i = 0; i < satirSayisi; i++)
+            {
+                for (int j = 0; j < satirSayisi - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < (2 * i) + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
